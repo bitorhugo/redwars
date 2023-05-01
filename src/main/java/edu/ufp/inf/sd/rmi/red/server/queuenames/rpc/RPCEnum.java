6b7cd2b1f20f8cf089926009bfa8e4909aa19f75
bitorhugo <vitorhugosantos144@gmail.com>
2023-05-01 15:53:49 +0100

    ADD: RPCENum

---
+++ src/main/java/edu/ufp/inf/sd/rmi/red/server/queuenames/rpc/RPCEnum.java
@@ -0,0 +1,21 @@
+package edu.ufp.inf.sd.rmi.red.server.queuenames.rpc;
+
+public enum RPCEnum {
+    
+    RPC_SEARCH_LOBBIES("rpc-search-lobbies"),
+    RPC_GET_PLAYERS("rpc-get-players"),
+    RPC_START_GAME("rpc-start-game"),
+    RPC_START_GAME_GUI("rpc-start-game-gui"),
+    RPC_LOGIN("rpc-login");
+    
+    private String value;
+
+    public String getValue() {
+        return this.value;
+    }
+    
+    private RPCEnum(String value) {
+        this.value = value;
+    }
+    
+}   
